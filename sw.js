7520a941fafb704df13eae7c1f12962201087844
Productions iMédias <philippe.beaubien@gmail.com>
2026-08-07 16:34:02 -0400

    Freemium: 3 modules gratuits (essai 7j), reste verrouille sans licence + bump SW

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Au premier chargement, il met en cache tous les fichiers de l'app.
    Ensuite, l'app se lance sans réseau, exactement comme une app native. */
 
-const CACHE = "coiffurequest-v2";
+const CACHE = "coiffurequest-v3";
 const ASSETS = [
   "index.html",
   "app.js",
